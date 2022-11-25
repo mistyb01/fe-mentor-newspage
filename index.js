eda5575d81f6eba61380551dcd757e4e4bf6b308
Misty Becker <mistyb01@uw.edu>
2022-11-24 20:31:17 -0800

    dark verlay works

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ let toggleIcon = document.getElementById('menu-icon');
 let navLinks = document.querySelector('.nav-menu');
 toggleIcon.onclick = function() {
     navLinks.classList.toggle('visible-menu');
+    let overlay = document.getElementById('overlay');
+    overlay.classList.toggle('overlay-effect');
 }
