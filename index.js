7ac53e8b381f929843f10f33e5ba61722c711744
Misty Becker <mistyb01@uw.edu>
2022-11-24 21:08:50 -0800

    icon image toggles properly

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ toggleIcon.onclick = function() {
     navLinks.classList.toggle('visible-menu');
     let overlay = document.getElementById('overlay');
     overlay.classList.toggle('overlay-effect');
+    
+    if (toggleIcon.getAttribute('src') === './assets/images/icon-menu.svg') {
+        toggleIcon.setAttribute('src','./assets/images/icon-menu-close.svg');
+    } else {
+        toggleIcon.setAttribute('src','./assets/images/icon-menu.svg');
+    }
 }
