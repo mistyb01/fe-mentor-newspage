592b9e8679cda2cc277c9c15c27286bdd0a2157c
Misty Becker <mistyb01@uw.edu>
2022-11-25 07:57:13 -0800

    removes menu nav styles on screen size change

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let toggleIcon = document.getElementById('menu-icon');
 let navLinks = document.querySelector('.nav-menu');
+
 toggleIcon.onclick = function() {
     navLinks.classList.toggle('visible-menu');
     let overlay = document.getElementById('overlay');
@@ -10,4 +11,13 @@ toggleIcon.onclick = function() {
     } else {
         toggleIcon.setAttribute('src','./assets/images/icon-menu.svg');
     }
+}
+
+window.onresize = function() {
+    const mediaQueryLarge = window.matchMedia('(min-width: 768px)');
+    if (mediaQueryLarge.matches) {
+        navLinks.classList.remove('visible-menu');
+        overlay.classList.remove('overlay-effect');
+    }
+    toggleIcon.setAttribute('src','./assets/images/icon-menu.svg');
 }
